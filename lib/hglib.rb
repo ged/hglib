bd3f2ae4b30abe52eab4a9abc74c18c93eb06f4e
Michael Granger <ged@FaerieMUD.org>
2018-01-24 08:53:22 -0800

    Add configurable binpath to the top-level module

--- lib/hglib.rb
+++ lib/hglib.rb
@@ -1,6 +1,8 @@
 # -*- ruby -*-
 #encoding: utf-8
 
+require 'pathname'
+
 # Toplevel namespace
 module Hglib
 
@@ -10,6 +12,26 @@ module Hglib
 	# Version control revision
 	REVISION = %q$Revision$
 
+	# The default path to the `hg` command
+	DEFAULT_HG_PATH = begin
+		paths = ENV['PATH'].
+			split( File::PATH_SEPARATOR ).
+			map {|dir| Pathname(dir) + 'hg' }
+
+		paths.find( &:executable? ) || Pathname( '/usr/bin/hg' )
+	end
+
+
+	### Return the currently-configured path to the `hg` binary./
+	def self::hg_path
+		return @hg_path ||= DEFAULT_HG_PATH
+	end
+
+
+	### Set the path to the `hg` binary that will be used for any new commands.
+	def self::hg_path=( new_path )
+		@hg_path = Pathname( new_path )
+	end
 
 end # module Hglib
 
--- spec/hglib_spec.rb
+++ spec/hglib_spec.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/env rspec -cfd
-#encoding: utf-8
 
 require_relative 'spec_helper'
 
-require 'rspec'
 require 'hglib'
+
 
 describe Hglib do
 
-	
+	before( :each ) do
+		@real_hg_path = described_class.hg_path
+	end
+
+	after( :each ) do
+		described_class.hg_path = @real_hg_path
+	end
+
+
+	it "has a default hg binary path" do
+		expect( described_class::DEFAULT_HG_PATH ).to be_a( Pathname )
+		expect( described_class::DEFAULT_HG_PATH.to_s ).to end_with( '/hg' )
+	end
+
+
+	it "can be configured to use a different hg binary" do
+		expect {
+			described_class.hg_path = '/somewhere/else/hg'
+		}.to change { described_class.hg_path }
+	end
 
 end
 
