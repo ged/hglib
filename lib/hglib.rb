21ebd6025245303d5d37d400ee0ac413bbb27e18
Michael Granger <ged@FaerieMUD.org>
2018-01-24 08:25:21 -0800

    Initial commit.

---
+++ lib/hglib.rb
@@ -0,0 +1,15 @@
+# -*- ruby -*-
+#encoding: utf-8
+
+# Toplevel namespace
+module Hglib
+
+	# Package version
+	VERSION = '0.0.1'
+
+	# Version control revision
+	REVISION = %q$Revision$
+
+
+end # module Hglib
+
---
+++ spec/hglib_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env rspec -cfd
+#encoding: utf-8
+
+require_relative 'spec_helper'
+
+require 'rspec'
+require 'hglib'
+
+describe Hglib do
+
+	
+
+end
+
