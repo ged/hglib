98ff2e7dfcf3687af1a7df26bc88823df58d7874
Michael Granger <ged@FaerieMUD.org>
2019-10-12 10:55:25 -0700

    Make gem.deps.rb rubygems-friendly

--- gem.deps.rb
+++ gem.deps.rb
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'loggability', '~> 0.11'
 
-gem 'rake-deveiate', '~> 0.1', :development
-gem 'simplecov', '~> 0.7', :development
-gem 'rdoc-generator-sixfish', '~> 0', :development
+group :development do
+	gem 'rake-deveiate', '~> 0.1'
+	gem 'simplecov', '~> 0.7'
+	gem 'rdoc-generator-fivefish', '~> 0.4'
+end
+
